8c45a04e8bd9db09452a1dce2b83b54bc46414f7
Simon J Stuart <lakraven@gmail.com>
2026-08-27 18:42:18 +0200

    Use neutral entropy provider include in ESP32 umbrella (#5)

--- src/ESPressio_ESP32.hpp
+++ src/ESPressio_ESP32.hpp
@@ -6,7 +6,7 @@
 #include "ESPressio_ESP32QueueProvider.hpp"
 #include "ESPressio_ESP32ClockProvider.hpp"
 #include "ESPressio_ESP32GPIOProvider.hpp"
-#include "ESPressio_ESP32EntropyProvider.hpp"
+#include "ESPressio_EntropyProvider.hpp"
 
 #if defined(ARDUINO)
 #include "ESPressio_ArduinoGPIOProvider.hpp"
