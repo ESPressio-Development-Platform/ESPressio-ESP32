a694f9efb3473f837edba2e14cf93fb096764670
Simon J Stuart <lakraven@gmail.com>
2026-08-27 17:56:25 +0200

    feat(esp32): expose and install complete System provider set (#5)

--- src/ESPressio_ESP32.hpp
+++ src/ESPressio_ESP32.hpp
@@ -1,2 +1,23 @@
 #pragma once
+
 #include "ESPressio_ESP32MemoryProvider.hpp"
+#include "ESPressio_ESP32ExecutionProvider.hpp"
+#include "ESPressio_ESP32SynchronizationProvider.hpp"
+#include "ESPressio_ESP32ClockProvider.hpp"
+#include "ESPressio_ESP32GPIOProvider.hpp"
+
+#if defined(ARDUINO)
+#include "ESPressio_ArduinoGPIOProvider.hpp"
+#endif
+
+namespace ESPressio::ESP32Platform {
+
+inline void InstallSystemProviders() noexcept {
+    InstallMemoryProvider();
+    InstallExecutionProvider();
+    InstallSynchronizationProvider();
+    InstallClockProviders();
+    InstallGPIOController();
+}
+
+} // namespace ESPressio::ESP32Platform
