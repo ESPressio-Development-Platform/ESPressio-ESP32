63c201ec0d1ba893b5e8b3712af4a8f0cc846e21
Simon J Stuart <lakraven@gmail.com>
2026-08-27 19:56:24 +0200

    fix(esp32): keep third-party socket adapters optional (#5, #32)

--- src/ESPressio_ESP32SocketTransports.hpp
+++ src/ESPressio_ESP32SocketTransports.hpp
@@ -4,6 +4,12 @@
 #include "ESPressio_TCPClientEventTransport.hpp"
 #include "ESPressio_TCPServerEventTransport.hpp"
 #include "ESPressio_TLSEventTransport.hpp"
+
+#if __has_include(<WebSocketsClient.h>) && __has_include(<WebSocketsServer.h>)
 #include "ESPressio_WebSocketClientEventTransport.hpp"
 #include "ESPressio_WebSocketServerEventTransport.hpp"
+#endif
+
+#if __has_include(<PubSubClient.h>)
 #include "ESPressio_MQTTEventTransport.hpp"
+#endif
