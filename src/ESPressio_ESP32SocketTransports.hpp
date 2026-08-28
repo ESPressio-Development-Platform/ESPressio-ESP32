1d7ceb3120ee14707f7da65c82c5fbeab8758637
Simon J Stuart <lakraven@gmail.com>
2026-08-28 10:25:06 +0200

    refactor(esp32): remove WebSocket Event transports from socket umbrella (#6, ESPressio-Web#11)

--- src/ESPressio_ESP32SocketTransports.hpp
+++ src/ESPressio_ESP32SocketTransports.hpp
@@ -5,11 +5,6 @@
 #include "ESPressio_TCPServerEventTransport.hpp"
 #include "ESPressio_TLSEventTransport.hpp"
 
-#if __has_include(<WebSocketsClient.h>) && __has_include(<WebSocketsServer.h>)
-#include "ESPressio_WebSocketClientEventTransport.hpp"
-#include "ESPressio_WebSocketServerEventTransport.hpp"
-#endif
-
 #if __has_include(<PubSubClient.h>)
 #include "ESPressio_MQTTEventTransport.hpp"
 #endif
