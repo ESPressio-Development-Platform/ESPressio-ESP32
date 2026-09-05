afece6d1b703b27b47d45981f6da1d24fdcf2320
Simon J Stuart <lakraven@gmail.com>
2026-09-05 20:25:58 +0200

    Validate ESP32 whole-device Mesh accounting

--- examples/ProviderSmoke/src/main.cpp
+++ examples/ProviderSmoke/src/main.cpp
@@ -1,12 +1,30 @@
 #include <Arduino.h>
+#include <array>
 
 #include <ESPressio_ESP32.hpp>
+#include <ESPressio_MeshMemoryAccounting.hpp>
 
 using MeshCapacityProfile = ESPressio::ESP32Platform::InternalMemoryMeshCapacityProfile<8192U, 16384U>;
 static_assert(MeshCapacityProfile::Identifier == 0x45533332U);
 static_assert(MeshCapacityProfile::InboundDeliveryPool::MaximumBytesPerSlot == 4096U);
 static_assert(MeshCapacityProfile::ControlFramePool::MaximumBytesPerSlot == 512U);
 static_assert(MeshCapacityProfile::ApplicationPayloadPool::MaximumBytesPerSlot == 3584U);
+
+struct MeshAccountingTopologyCharacteristics final {
+    std::int16_t SignalDbm{0};
+    std::uint16_t CostHint{0};
+};
+struct MeshAccountingClockQuality final { std::uint32_t UncertaintyNanoseconds{0}; };
+struct MeshAccountingSecurityAuthority final { std::array<std::uint8_t, 1024> BoundedState{}; };
+using MeshAccounting = ESPressio::Mesh::MeshWholeDeviceMemoryAccounting<
+    MeshAccountingTopologyCharacteristics,
+    MeshAccountingClockQuality,
+    8U,
+    MeshCapacityProfile,
+    MeshAccountingSecurityAuthority
+>;
+static_assert(MeshAccounting::RadioReassemblyPayloadBytes == 4U * 4096U);
+static_assert(MeshAccounting::TotalAccountedBytes > MeshAccounting::MeshPrincipalBytes);
 #include <ESPressio_Memory.hpp>
 #include <ESPressio_Radio.hpp>
 #include <ESPressio_Raw80211Radio.hpp>
