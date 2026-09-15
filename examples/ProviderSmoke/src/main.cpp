d97505d765835be722d514126d42333964d7a5d9
Simon Stuart <lakraven@gmail.com>
2026-09-15 10:33:10 +0200

    Exercise ESP32 OTA Platform and Security providers

--- examples/ProviderSmoke/src/main.cpp
+++ examples/ProviderSmoke/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <sdkconfig.h>
 
+#include <array>
+#include <cstddef>
+
 #include <ESPressio_ESP32.hpp>
 #include <ESPressio_Radio.hpp>
 
@@ -8,6 +11,45 @@ namespace {
 ESPressio::ESP32Platform::Raw80211Radio rawRadio;
 #if defined(CONFIG_BT_BLE_ENABLED) && CONFIG_BT_BLE_ENABLED && defined(CONFIG_BT_BLUEDROID_ENABLED) && CONFIG_BT_BLUEDROID_ENABLED
 ESPressio::ESP32Platform::BLERadio bleRadio;
+#endif
+
+#if __has_include(<ESPressio_Platform_IDFOTA.hpp>)
+ESPressio::ESP32Platform::OTAApplicationImageStaging otaApplicationStaging;
+ESPressio::ESP32Platform::OTABootControl otaBootControl;
+ESPressio::ESP32Platform::OTATrialBoot otaTrialBoot;
+ESPressio::ESP32Platform::OTASystemRestart otaSystemRestart;
+ESPressio::ESP32Platform::OTAStorageLayoutInspection otaStorageLayoutInspection;
+static_assert(ESPressio::Platform::OTA::IsApplicationImageStagingProviderV<
+              ESPressio::ESP32Platform::OTAApplicationImageStaging>);
+static_assert(ESPressio::Platform::OTA::IsBootControlProviderV<
+              ESPressio::ESP32Platform::OTABootControl>);
+static_assert(ESPressio::Platform::OTA::IsTrialBootProviderV<
+              ESPressio::ESP32Platform::OTATrialBoot>);
+static_assert(ESPressio::Platform::OTA::IsSystemRestartProviderV<
+              ESPressio::ESP32Platform::OTASystemRestart>);
+static_assert(ESPressio::Platform::OTA::IsStorageLayoutInspectionProviderV<
+              ESPressio::ESP32Platform::OTAStorageLayoutInspection>);
+#endif
+
+#if __has_include(<ESPressio_OTACapacityProfile.hpp>)
+static_assert(ESPressio::ESP32Platform::ESP32OTACapacityProfile::IsValid);
+static_assert(ESPressio::ESP32Platform::ESP32OTACapacityProfile::TransferBufferBytes == 4096U);
+#endif
+
+#if __has_include(<ESPressio_Verification.hpp>) && __has_include(<psa/crypto.h>)
+ESPressio::ESP32Platform::OTASHA256 otaSHA256;
+ESPressio::ESP32Platform::OTAECDSAP256SHA256SignatureVerifier otaSignatureVerifier;
+constexpr std::array<std::uint8_t, 65U> otaSmokePublicKey = [] {
+    std::array<std::uint8_t, 65U> key{};
+    key[0] = 0x04U;
+    return key;
+}();
+constexpr ESPressio::Security::TrustAnchorIdentifier otaSmokeAnchorId{1U};
+constexpr ESPressio::Security::TrustPolicyIdentifier otaSmokePolicyId{1U};
+ESPressio::ESP32Platform::OTAImmutableTrustAnchor otaTrustAnchor{
+    otaSmokeAnchorId, otaSmokePublicKey.data(), otaSmokePublicKey.size()};
+ESPressio::ESP32Platform::OTAExactManifestTrustPolicy otaTrustPolicy{
+    otaSmokePolicyId, otaSmokeAnchorId};
 #endif
 }
 
@@ -29,6 +71,32 @@ void setup() {
     (void)rawFiniteIngress;
     (void)rawDomainValid;
     (void)rawCostValid;
+
+#if __has_include(<ESPressio_Platform_IDFOTA.hpp>)
+    // Construction plus compile-time provider traits are the smoke target here;
+    // do not mutate flash/boot state merely to prove the integration compiles.
+    volatile std::size_t otaPlatformProviderBytes =
+        sizeof(otaApplicationStaging) + sizeof(otaBootControl) + sizeof(otaTrialBoot) +
+        sizeof(otaSystemRestart) + sizeof(otaStorageLayoutInspection);
+    (void)otaPlatformProviderBytes;
+#endif
+
+#if __has_include(<ESPressio_Verification.hpp>) && __has_include(<psa/crypto.h>)
+    volatile bool sha256Supported = otaSHA256.Supports(ESPressio::Security::DigestAlgorithm::SHA256);
+    volatile bool signatureSupported = otaSignatureVerifier.Supports(
+        ESPressio::ESP32Platform::OTASecurityAlgorithm::ECDSA_SHA256_P256);
+    ESPressio::Security::TrustAnchorView anchor{};
+    const auto anchorResult = otaTrustAnchor.Resolve(otaSmokeAnchorId, anchor);
+    const auto policyResult = otaTrustPolicy.Authorize(
+        otaSmokePolicyId,
+        ESPressio::Security::TrustPurpose::SoftwareUpdateManifest,
+        otaSmokeAnchorId);
+    volatile bool trustBound = static_cast<bool>(anchorResult) && static_cast<bool>(policyResult) &&
+                               static_cast<bool>(anchor);
+    (void)sha256Supported;
+    (void)signatureSupported;
+    (void)trustBound;
+#endif
 
 #if defined(CONFIG_BT_BLE_ENABLED) && CONFIG_BT_BLE_ENABLED && defined(CONFIG_BT_BLUEDROID_ENABLED) && CONFIG_BT_BLUEDROID_ENABLED
     const auto bleCapabilities = bleRadio.Capabilities();
